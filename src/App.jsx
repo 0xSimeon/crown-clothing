a03327931e969ed9ab0c6295ac554f754c368df1
0xSimeon <simeon.udoh45@gmail.com>
2023-04-02 04:49:33 +0100

    add project scaffold :tada:

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+      {/* {img />}  */}
+      <div className="category-body-container">
+        <h2>Hats</h2>
+        <p>Shop now</p>
+      </div>
+      </div>
+
+      <div className="category-container">
+      {/* {img />}  */}
+      <div className="category-body-container">
+        <h2>Jackets</h2>
+        <p>Shop now</p>
+      </div>
+      </div>
+
+      <div className="category-container">
+      {/* {img />}  */}
+      <div className="category-body-container">
+        <h2>Sneakers</h2>
+        <p>Shop now</p>
+      </div>
+      </div>
+
+      <div className="category-container">
+      {/* {img />}  */}
+      <div className="category-body-container">
+        <h2>Women</h2>
+        <p>Shop now</p>
+      </div>
+      </div>
+
+      <div className="category-container">
+      {/* {img />}  */}
+      <div className="category-body-container">
+        <h2>Mens</h2>
+        <p>Shop now</p>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
