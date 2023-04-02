eb1c6e01cb3fcdf63eb53e8fc0761f70b7d70d4b
0xSimeon <simeon.udoh45@gmail.com>
2023-04-02 05:16:50 +0100

    :lipstick: add sass package and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,48 @@
 import { useState } from "react";
+import "./categories.styles.scss";
 
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+
+    {
+      id: 2,
+      title: "Jackets",
+    },
+
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+
+    {
+      id: 4,
+      title: "Women",
+    },
+
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
   return (
     <div className="categories-container">
-      <div className="category-container">
-      {/* {img />}  */}
-      <div className="category-body-container">
-        <h2>Hats</h2>
-        <p>Shop now</p>
-      </div>
-      </div>
+      {categories.map((category) => {
+        const { id, title } = category;
 
-      <div className="category-container">
-      {/* {img />}  */}
-      <div className="category-body-container">
-        <h2>Jackets</h2>
-        <p>Shop now</p>
-      </div>
-      </div>
-
-      <div className="category-container">
-      {/* {img />}  */}
-      <div className="category-body-container">
-        <h2>Sneakers</h2>
-        <p>Shop now</p>
-      </div>
-      </div>
-
-      <div className="category-container">
-      {/* {img />}  */}
-      <div className="category-body-container">
-        <h2>Women</h2>
-        <p>Shop now</p>
-      </div>
-      </div>
-
-      <div className="category-container">
-      {/* {img />}  */}
-      <div className="category-body-container">
-        <h2>Mens</h2>
-        <p>Shop now</p>
-      </div>
-      </div>
+        return (
+          <div className="category-container" key={id}>
+            <div className="background-image"></div>
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
